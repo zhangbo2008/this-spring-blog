3b55d44c570dc79e963e83799c88f4cae66645a3
xuxiuquan@kaochong.com <xuxiuquan@kaochong.com>
2020-03-22 17:50:25 +0800

    add: node module load

---
+++ node/1/1.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: xiuquanxu
+ * @Company: kaochong
+ * @Date: 2020-03-20 19:47:05
+ * @LastEditors: xiuquanxu
+ * @LastEditTime: 2020-03-22 16:33:09
+ */
+const md = require('module');
+
+// md._resolveFilename = function(x, y, z, m) {
+//     console.log(x, y, z, m);
+// }
+// md._findPath = function(x, y, z, m) {
+//     console.log(x, y, z, m);
+// }
+const h = require('hhhttp');
+// console.log(JSON.stringify(h));
+// console.log(md._cache);
+
--- node/catalog.md
+++ node/catalog.md
@@ -3,12 +3,12 @@
  * @Company: kaochong
  * @Date: 2020-03-09 00:17:25
  * @LastEditors: xiuquanxu
- * @LastEditTime: 2020-03-18 22:03:08
+ * @LastEditTime: 2020-03-22 16:39:49
  -->
-## 介绍npm & npx & nvm & yarn  
+# NODE学习记录    
+
+## 1.md-源码角度分析nodejs模块加载
 
 <a href="./1.md">跳转</a>  
 
-## 浏览器缓存  
 
-<a href="./2,md">跳转</a>
