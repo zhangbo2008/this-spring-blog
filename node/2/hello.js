c9ae6f2ff9e7222c3705778636d0ca8dd49e4a0a
xuxiuquan@kaochong.com <xuxiuquan@kaochong.com>
2020-04-07 23:28:33 +0800

    add: pcm recorder player blog

---
+++ node/2/hello.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: xiuquanxu
+ * @Company: kaochong
+ * @Date: 2020-03-30 10:34:00
+ * @LastEditors: xiuquanxu
+ * @LastEditTime: 2020-03-30 10:35:13
+ */
+var add = require('bindings')('hello');
+
+console.log(add.hello());
