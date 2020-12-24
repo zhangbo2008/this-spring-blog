77952626eeab8a83354438058df3e22c9b2db639
xuxiuquan@kaochong.com <xuxiuquan@kaochong.com>
2020-12-24 14:02:06 +0800

    add: string.js

---
+++ offer/face/kuaishou/acfun.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: xiuquanxu
+ * @Company: kaochong
+ * @Date: 2020-12-23 23:58:20
+ * @LastEditors: xiuquanxu
+ * @LastEditTime: 2020-12-23 23:58:21
+*/
+// 1. console.log(typeof typeof typeof null) // string
+// 2. 'abcdcba'.replace(/c/, "h");
+function change(str) {
+    
+}
---
+++ offer/正则表达式.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: xiuquanxu
+ * @Company: kaochong
+ * @Date: 2020-12-24 13:19:23
+ * @LastEditors: xiuquanxu
+ * @LastEditTime: 2020-12-24 13:19:24
+ */
